ffbafe9f294fd40ffc360098f2e700b05027bad6
root <root@ip-172-31-45-153.eu-west-3.compute.internal>
2018-11-18 20:14:16 +0000

    1811

--- adhoc-docker/node.sh
+++ adhoc-docker/node.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
-echo "set Autobootstrap FALSE"
-echo "auto_bootstrap: false" >>/etc/cassandra/cassandra.yaml
-echo "register node [$WHOAMI]"
+echo "create node [$WHOAMI]"
+echo "set Autobootstrap [$AUTO]"
+if [ "$AUTO" = false ] ; then
+    	echo "AUTO_BOOTSTRAP is set to false"
+	echo "auto_bootstrap: false" >>/etc/cassandra/cassandra.yaml
+fi
+#echo "set Autobootstrap FALSE"
+#echo "auto_bootstrap: false" >>/etc/cassandra/cassandra.yaml
+
 while ! cqlsh -e 'describe cluster' ; do
  sleep 1
 done
-#echo "...dormo un rato..."
-#sleep 40
-#echo "desperto"
+echo "action register node [$WHOAMI] "
 cqlsh -e "INSERT into adhoc_cloud.nodes(id,ip) values (uuid(), '$WHOAMI');"
 cqlsh -e "SELECT * FROM adhoc_cloud.nodes" > /usr/src/nodes.txt
 cat /usr/src/nodes.txt
+echo "bye, node [$WHOAMI] registered ..."
+
+
+
+
+
+
 echo "bye, node [$WHOAMI] created..."
