c6445d130b3a0e0d2da983042947a0406c59c451
Ana Juan <anajuanferrer@gmail.com>
2018-11-20 18:26:46 +0100

    2011-2

--- etcd/adhoc-cloud-etcd.sh
+++ etcd/adhoc-cloud-etcd.sh
@@ -10,7 +10,10 @@ case $1 in
 	echo "EXECUTO --- docker run -m $memLimit -d -p 4001:4001 -p 2380:2380 -p 2379:2379 --net=host --name etcd0  $etcd_docker -name etcd0  -advertise-client-urls http://localhost:2379,http://localhost:4001  -listen-client-urls http://localhost:2379,http://localhost:4001  -initial-advertise-peer-urls http://localhost:2380  -listen-peer-urls http://localhost:2380  -initial-cluster-token etcd-cluster-1  -initial-cluster etcd0=http://localhost:2380"
 	docker run -m $memLimit -d -p 4001:4001 -p 2380:2380 -p 2379:2379 --net=host --name etcd0  $etcd_docker -name etcd0  -advertise-client-urls http://localhost:2379,http://localhost:4001  -listen-client-urls http://localhost:2379,http://localhost:4001  -initial-advertise-peer-urls http://localhost:2380  -listen-peer-urls http://localhost:2380  -initial-cluster-token etcd-cluster-1  -initial-cluster etcd0=http://localhost:2380
 	master_ip=`docker inspect --format='{{ .NetworkSettings.IPAddress }}' etcd0`
-        echo "HEALTH" 
+        
+	#echo "set value" 
+	#curl http://127.0.0.1:2379/v2/keys/adhoc-cloud/etcd0 -XPUT -d value="etcd0"
+	echo "HEALTH" 
 	curl -L http://127.0.0.1:2379/health
 	echo "MEMBERS"
 	curl -L http://127.0.0.1:2379/v2/members
@@ -26,13 +29,13 @@ case $1 in
 	for ((i=0; i<$2; i++))
 	do
 		nom="etcd$i=http://localhost:$port2";
-		echo $nom
+		#echo $nom
 		if [ "$initialClusterStr" != "" ]
 		then
     		 initialClusterStr="$initialClusterStr,"
 		fi
     		initialClusterStr="$initialClusterStr$nom"
-		echo $initialClusterStr
+		#echo $initialClusterStr
                 port2=`expr $port2 + 100`
 	done
         port1=4001
@@ -42,15 +45,15 @@ case $1 in
 	do
            data_iniIns=`date +%s`
       	   echo "setup ETCD$i"
-	   echo " Port 1[$port1] Port2[$port2] Port3[$port3]"
+	   #echo " Port 1[$port1] Port2[$port2] Port3[$port3]"
 	  
-	   echo "EXECUTO === docker run -m $memLimit -d -p $port1:$port1 -p $port2:$port2 -p $port3:$port3 --net=host --name etcd$i  $etcd_docker -name etcd$i \
-                -advertise-client-urls http://localhost:$port3,http://localhost:$port1\
-                -listen-client-urls http://localhost:$port3,http://localhost:$port1\
-                -initial-advertise-peer-urls http://localhost:$port2\
-                -listen-peer-urls http://localhost:$port2\
-                -initial-cluster-token etcd-cluster-1  \
-                -initial-cluster $initialClusterStr"
+	   #echo "EXECUTO === docker run -m $memLimit -d -p $port1:$port1 -p $port2:$port2 -p $port3:$port3 --net=host --name etcd$i  $etcd_docker -name etcd$i \
+           #     -advertise-client-urls http://localhost:$port3,http://localhost:$port1\
+           #     -listen-client-urls http://localhost:$port3,http://localhost:$port1\
+           #     -initial-advertise-peer-urls http://localhost:$port2\
+           #     -listen-peer-urls http://localhost:$port2\
+           #     -initial-cluster-token etcd-cluster-1  \
+           #     -initial-cluster $initialClusterStr"
 	   docker run -m $memLimit -d -p $port1:$port1 -p $port2:$port2 -p $port3:$port3 --net=host --name etcd$i  $etcd_docker -name etcd$i \
                 -advertise-client-urls http://localhost:$port3,http://localhost:$port1\
                 -listen-client-urls http://localhost:$port3,http://localhost:$port1\
@@ -58,22 +61,36 @@ case $1 in
                 -listen-peer-urls http://localhost:$port2\
                 -initial-cluster-token etcd-cluster-1  \
                 -initial-cluster $initialClusterStr
-		
-	        port1=`expr $port1 + 100`
-		port2=`expr $port2 + 100`
-		port3=`expr $port3 + 100`
+                
+                echo "STATS*****************************************************"
+	        stat=$(curl -L http://localhost:$port3/v2/stats/self)
+                echo "STATS*****************************************************"
 
-                data_end=`date +%s`
+		data_end=`date +%s`
                 ELAPSED_TIME=`expr $data_end - $data_ini`
                 ELAPSED_TIME_INS=`expr $data_end - $data_iniIns`
-                echo "CREAR $i \t $ELAPSED_TIME \t $ELAPSED_TIME_INS"
+                #echo "CREAR $i \t $ELAPSED_TIME \t $ELAPSED_TIME_INS"
                 echo -e "$i \t $ELAPSED_TIME \t $ELAPSED_TIME_INS">>$fileName
+                
+		port1=`expr $port1 + 100`
+                port2=`expr $port2 + 100`
+                port3=`expr $port3 + 100`
 	done
- 
+
+  	port3=2379
+        for ((i=0; i<$2; i++))
+        do
+	   echo "insert to i[$i] to[http://localhost:$port3/v2/keys/adhoc-cloud/ectd$i] val[etcd$i]"
+	   curl -L http://localhost:$port3/v2/keys/adhoc-cloud/ectd$i -XPUT -d value="etcd$i"
+           port3=`expr $port3 + 100`
+	done
+
         echo "HEALTH" 
-	curl -L http://127.0.0.1:2379/health
+	curl -L http://localhost:2379/health
 	echo "MEMBERS"
-	curl -L http://127.0.0.1:2379/v2/members
+	curl -L http://localhost:2379/v2/members
+	echo "DATA"
+	curl -L http://localhost:2379/v2/keys/adhoc-cloud
 	
 	data_end=`date +%s`
 	ELAPSED_TIME=`expr $data_end - $data_ini`
