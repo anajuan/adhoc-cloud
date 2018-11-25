73cf34ee588334e7763d08adcec2dc708f9c1c09
Ana Juan <anajuanferrer@gmail.com>
2018-11-25 11:59:02 +0100

    raspi 3

--- raspi/adhoc-cloud-etcd-rpi.sh
+++ raspi/adhoc-cloud-etcd-rpi.sh
@@ -4,20 +4,26 @@ etcd_docker="peterrosell/etcd-rpi:2.3.7"
 memLimit="32M"
 case $1 in
 "member")
-	IP=$2
-	nom=$3
-	initialCluster=$4
-	echo "Params IP[$IP] Nom[$nom] clusterMembers[$initialCluster]"
-	#if [ "$IP" == "" ]; then
+	if [ $2 ]; then
+		IP=$2
+	else
+		IP="default"
+	fi
+
+	if [ $3 ]; then
+		nom=$3
+	else
+		nom="default"
+	fi
+	if [ $4 ]; then
+		initialCluster=$4
+	else
+		initialCluster="default"
+	fi
+	echo "cluster_member_create) Params IP[$IP] Nom[$nom] clusterMembers[$initialCluster] "
 	#	IP=`ip -4 route get 8.8.8.8 | awk {'print $7'} | tr -d '\n'`
-	#fi
-	#if [ "$nom" == "" ]; then
 	#	nom="etcd"
-	#fi
-	#if [ "$initialCluster" == "" ]; then
 	#	initialCluster="$nom=http://$IP:2380"
-	#fi
-	#echo "Init IP[$IP] Nom[$nom] clusterMembers[$initialCluster]"
 	
 	docker run --env GOMAXPROCS=4 \
 		-e ETCD_UNSUPPORTED_ARCH=arm \
@@ -34,24 +40,31 @@ case $1 in
 		-listen-peer-urls http://$IP:2380 \
 		-initial-cluster-token etcd-cluster-1 \
 		-initial-cluster $initialCluster 
-	echo "work done, bye"
 	;;
 "cluster")
-       	echo "cluster"
+	if [ $3 ]; then
+		./sync-rpi.sh
+	fi
 	WHO=$2
-	echo "WHO $WHO"
+	echo "cluster_create) WHO $WHO"
 	IFS=,
 	ary=($WHO)
 	initialClusterStr=""
-	
+	clusterSize=${#ary[@]}
+	echo "cluster_create) size is [$clusterSize]"
+        declare -a IPs
+	data_ini=`date +%s`
+	fileName="data/data-rpiEtcdCluster-$data_ini-$clusterSize.dat"
+	echo -e "Nodes $WHO">>$fileName
+	echo -e "Name \t Num \t Acumtime \t TimeIns">>$fileName
+        ### Step 1: Create the initialCluster parameter	
+	initialClusterStr=""
 	for key in "${!ary[@]}"; do 
-		#####echo "$key ${ary[$key]}";
 		node=${ary[$key]}
 		num=$key
-		#####echo "Node[$node]"
-		#####echo "Num [$num]"
 		IP_Node=$(ssh pi@$node 'ip -4 route get 8.8.8.8'|awk {'print $7'} | tr -d '\n')
-		#####echo "IP[$IP_Node]"	
+		IPs[num]=$IP_Node
+		#####echo "IP Array Step1 ${IPs[num]}"
 		nom="etcd$num=http://$IP_Node:2380";
 		if [ "$initialClusterStr" != "" ]
 		then
@@ -60,47 +73,76 @@ case $1 in
     		initialClusterStr="$initialClusterStr$nom"
 	done
 	
+        ### Step 2: Instatiate etcd container in each rpi hosts 	
 	for key in "${!ary[@]}"; do 
-		echo "$key ${ary[$key]}";
+                data_iniIns=`date +%s`
 		node=${ary[$key]}
 		num=$key
-		echo "Node[$node]"
-		echo "Num [$num]"
-		IP_Node=$(ssh pi@$node 'ip -4 route get 8.8.8.8'|awk {'print $7'} | tr -d '\n')
+		###IP_Node=$(ssh pi@$node 'ip -4 route get 8.8.8.8'|awk {'print $7'} | tr -d '\n')
+		IP_Node=${IPs[num]}
+		echo "IP Array Step2 ${IPs[num]} IP_Node $IP_Node"
 		nom="etcd$num"
-		echo "Invoke ---------SSH[ ssh pi@$node '/home/pi/adhoc-cloud/raspi/adhoc-cloud-etcd-rpi.sh member $IP_Node $nom $initialClusterStr'] IP[$IP_Node] Nom[$nom] initialCluster[$initialClusterStr]"
+		value="$node#$IP_Node"
                 ssh pi@$node "/home/pi/adhoc-cloud/raspi/adhoc-cloud-etcd-rpi.sh member $IP_Node $nom $initialClusterStr" 
-	        curl -L http://$IP_Node:2379/v2/stats/self
-	        curl -L http://$IP_Node:2379/v2/members
+		data_end=`date +%s`
+                ELAPSED_TIME=`expr $data_end - $data_ini`
+                ELAPSED_TIME_INS=`expr $data_end - $data_iniIns`
+                echo -e "$node \t $num \t $ELAPSED_TIME \t $ELAPSED_TIME_INS">>$fileName
+		echo "cluster_create) Num [$num] Node[$node] IP[$IP_Node] InstanceTime[$ELAPSED_TIME_INS] STimeFromStart[$ELAPSED_TIME]"
+		nodeStats=$(curl -sS http://$IP_Node:2379/v2/stats/self)
+		echo "cluster_create) Node Stats $nodeStats"
+	done
 
-	done
+        ### Step 3: Insert each value in keyspace. This can not be done until the full cluster is instantiated.  	
+	for key in "${!ary[@]}"; do 
+		node=${ary[$key]}
+		num=$key
+		#IP_Node=$(ssh pi@$node 'ip -4 route get 8.8.8.8'|awk {'print $7'} | tr -d '\n')
+		IP_Node=${IPs[num]}
+		echo "IP Array Step3 ${IPs[num]} IP_Node $IP_Node "
+		nom="etcd$num"
+		value="$node#$IP_Node"
+	        insertValue=$(curl -sS http://$IP_Node:2379/v2/keys/adhoc-cloud/$nom -XPUT -d value="$value")
+	        echo "cluster_create) IntertValue $insertValue"
+        done
+	### Step 4: check cluster health, members and inserted keys.
+	firstNode=${ary[0]}
+	echo "IP Array Step 4${IPs[0]}"
+	IP_FirstNode=${IPs[num]}
+	#IP_FirstNode=$(ssh pi@$firstNode 'ip -4 route get 8.8.8.8'|awk {'print $7'} | tr -d '\n')
+	health=$(curl -sS http://$IP_FirstNode:2379/health)
+	echo "cluster_data) Health[$health]" 
+	members=$(curl -sS http://$IP_FirstNode:2379/v2/members)
+	echo "cluster_data) Members[$members]"
+	data=$(curl -sS http://$IP_FirstNode:2379/v2/keys/adhoc-cloud)
+	echo "cluster_data) DATA[$data]"
 	;;
-"nodes") 
-	data_ini=`date +%s`
-	fileName="data/data-addNodesETCD-$data_ini-$2.dat"
-	echo -e "num \t Acumtime \t TimeIns">>$fileName
-	echo "$data_ini vols $2 fills"
-	initialClusterStr=""
-	port2=2380
-	for ((i=0; i<$2; i++))
-	do
-		nom="etcd$i=http://localhost:$port2";
-		#echo $nom
-		if [ "$initialClusterStr" != "" ]
-		then
-    		 initialClusterStr="$initialClusterStr,"
-		fi
-    		initialClusterStr="$initialClusterStr$nom"
-		#echo $initialClusterStr
-                port2=`expr $port2 + 100`
-	done
-        port1=4001
-	port2=2380
-	port3=2379	
-	for ((i=0; i<$2; i++))
-	do
-           data_iniIns=`date +%s`
-      	   echo "setup ETCD$i"
+#"nodes") 
+#	data_ini=`date +%s`
+#	fileName="data/data-addNodesETCD-$data_ini-$2.dat"
+#	echo -e "num \t Acumtime \t TimeIns">>$fileName
+#	echo "$data_ini vols $2 fills"
+#	initialClusterStr=""
+#	port2=2380
+#	for ((i=0; i<$2; i++))
+#	do
+#		nom="etcd$i=http://localhost:$port2";
+#		#echo $nom
+#		if [ "$initialClusterStr" != "" ]
+#		then
+#    		 initialClusterStr="$initialClusterStr,"
+#		fi
+#    		initialClusterStr="$initialClusterStr$nom"
+#		#echo $initialClusterStr
+#                port2=`expr $port2 + 100`
+#	done
+ #       port1=4001
+#	port2=2380
+#	port3=2379	
+#	for ((i=0; i<$2; i++))
+#	do
+  #         data_iniIns=`date +%s`
+ #     	   echo "setup ETCD$i"
 	   #echo " Port 1[$port1] Port2[$port2] Port3[$port3]"
 	  
 	   #echo "EXECUTO === docker run -m $memLimit -d -p $port1:$port1 -p $port2:$port2 -p $port3:$port3 --net=host --name etcd$i  $etcd_docker -name etcd$i \
@@ -110,47 +152,47 @@ case $1 in
            #     -listen-peer-urls http://localhost:$port2\
            #     -initial-cluster-token etcd-cluster-1  \
            #     -initial-cluster $initialClusterStr"
-	   docker run -m $memLimit -d -p $port1:$port1 -p $port2:$port2 -p $port3:$port3 --net=host --name etcd$i  $etcd_docker -name etcd$i \
-                -advertise-client-urls http://localhost:$port3,http://localhost:$port1\
-                -listen-client-urls http://localhost:$port3,http://localhost:$port1\
-                -initial-advertise-peer-urls http://localhost:$port2\
-                -listen-peer-urls http://localhost:$port2\
-                -initial-cluster-token etcd-cluster-1  \
-                -initial-cluster $initialClusterStr
-                
-                echo "STATS*****************************************************"
-	        stat=$(curl -L -Ss http://localhost:$port3/v2/stats/self)
-                echo "STATS*****************************************************"
-
-		data_end=`date +%s`
-                ELAPSED_TIME=`expr $data_end - $data_ini`
-                ELAPSED_TIME_INS=`expr $data_end - $data_iniIns`
+#	   docker run -m $memLimit -d -p $port1:$port1 -p $port2:$port2 -p $port3:$port3 --net=host --name etcd$i  $etcd_docker -name etcd$i \
+    #            -advertise-client-urls http://localhost:$port3,http://localhost:$port1\
+    #            -listen-client-urls http://localhost:$port3,http://localhost:$port1\
+    #            -initial-advertise-peer-urls http://localhost:$port2\
+   #             -listen-peer-urls http://localhost:$port2\
+  #              -initial-cluster-token etcd-cluster-1  \
+ #             -initial-cluster $initialClusterStr
+  #              
+ #               echo "STATS*****************************************************"
+#	        stat=$(curl -L -Ss http://localhost:$port3/v2/stats/self)
+ #               echo "STATS*****************************************************"
+#
+#		data_end=`date +%s`
+   #             ELAPSED_TIME=`expr $data_end - $data_ini`
+  #              ELAPSED_TIME_INS=`expr $data_end - $data_iniIns`
                 #echo "CREAR $i \t $ELAPSED_TIME \t $ELAPSED_TIME_INS"
-                echo -e "$i \t $ELAPSED_TIME \t $ELAPSED_TIME_INS">>$fileName
+ #               echo -e "$i \t $ELAPSED_TIME \t $ELAPSED_TIME_INS">>$fileName
                 
-		port1=`expr $port1 + 100`
-                port2=`expr $port2 + 100`
-                port3=`expr $port3 + 100`
-	done
+#		port1=`expr $port1 + 100`
+ #               port2=`expr $port2 + 100`
+ #               port3=`expr $port3 + 100`
+#	done
 
-  	port3=2379
-        for ((i=0; i<$2; i++))
-        do
-	   echo "insert to i[$i] to[http://localhost:$port3/v2/keys/adhoc-cloud/ectd$i] val[etcd$i]"
-	   curl -L http://localhost:$port3/v2/keys/adhoc-cloud/ectd$i -XPUT -d value="etcd$i"
-           port3=`expr $port3 + 100`
-	done
+ # 	port3=2379
+ #       for ((i=0; i<$2; i++))
+ #       do
+#	   echo "insert to i[$i] to[http://localhost:$port3/v2/keys/adhoc-cloud/ectd$i] val[etcd$i]"
+#	   curl -L http://localhost:$port3/v2/keys/adhoc-cloud/ectd$i -XPUT -d value="etcd$i"
+ #          port3=`expr $port3 + 100`
+#	done
 
-        echo "HEALTH" 
-	curl -L http://localhost:2379/health
-	echo "MEMBERS"
-	curl -L http://localhost:2379/v2/members
-	echo "DATA"
-	curl -L http://localhost:2379/v2/keys/adhoc-cloud
+#       echo "HEALTH" 
+#	curl -L http://localhost:2379/health
+#	echo "MEMBERS"
+#	curl -L http://localhost:2379/v2/members
+#	echo "DATA"
+#	curl -L http://localhost:2379/v2/keys/adhoc-cloud
 	
-	data_end=`date +%s`
-	ELAPSED_TIME=`expr $data_end - $data_ini`
-	echo -e "TOTAL$2 \t $ELAPSED_TIME">>$fileName
+#	data_end=`date +%s`
+#	ELAPSED_TIME=`expr $data_end - $data_ini`
+#	echo -e "TOTAL$2 \t $ELAPSED_TIME">>$fileName
 		
 	###echo "Before leaving. Sync all data..."
 	###for ((i=0;i<$2;i++))
@@ -164,8 +206,8 @@ case $1 in
 	###docker exec -i -t adhoc-cloud-master sh -c 'nodetool rebuild' 
 	###echo "END Before leaving. Sync all data..."
 
-	echo "bye"
-	;;
+#	echo "bye"
+#	;;
 *)
 	echo "k ase?"
 #if [ $1 == "setup" ]
---
+++ raspi/sync-rpi.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "raspberrypi-new1-eth0 =============> raspberrypi-new2-eth0"
+scp adhoc-cloud-etcd-rpi.sh pi@raspberrypi-new2-eth0:/home/pi/adhoc-cloud/raspi
+scp cleanup-etcd-rpi.sh pi@raspberrypi-new2-eth0:/home/pi/adhoc-cloud/raspi
+
+echo "raspberrypi-new1-eth0 =============> raspberrypi-new3-eth0"
+scp adhoc-cloud-etcd-rpi.sh pi@raspberrypi-new3-eth0:/home/pi/adhoc-cloud/raspi
+scp cleanup-etcd-rpi.sh pi@raspberrypi-new3-eth0:/home/pi/adhoc-cloud/raspi
+
+echo "raspberrypi-new1-eth0 =============> raspberrypi-new4-eth0"
+scp adhoc-cloud-etcd-rpi.sh pi@raspberrypi-new4-eth0:/home/pi/adhoc-cloud/raspi
+scp cleanup-etcd-rpi.sh pi@raspberrypi-new4-eth0:/home/pi/adhoc-cloud/raspi
+
+echo "raspberrypi-new1-eth0 =============> raspberrypi-jgn1-eth0"
+scp adhoc-cloud-etcd-rpi.sh pi@raspberrypi-jgn1-eth0:/home/pi/adhoc-cloud/raspi
+scp cleanup-etcd-rpi.sh pi@raspberrypi-jgn1-eth0:/home/pi/adhoc-cloud/raspi
+
+echo "raspberrypi-new1-eth0 =============> raspberrypi-jgn2-eth0"
+scp adhoc-cloud-etcd-rpi.sh pi@raspberrypi-jgn2-eth0:/home/pi/adhoc-cloud/raspi
+scp cleanup-etcd-rpi.sh pi@raspberrypi-jgn2-eth0:/home/pi/adhoc-cloud/raspi
+
+echo "raspberrypi-new1-eth0 =============> raspberrypi-old1-eth0"
+scp adhoc-cloud-etcd-rpi.sh pi@raspberrypi-old1-eth0:/home/pi/adhoc-cloud/raspi
+scp cleanup-etcd-rpi.sh pi@raspberrypi-old1-eth0:/home/pi/adhoc-cloud/raspi
+
+echo "raspberrypi-new1-eth0 =============> raspberrypi-old2-eth0"
+scp adhoc-cloud-etcd-rpi.sh pi@raspberrypi-old2-eth0:/home/pi/adhoc-cloud/raspi
+scp cleanup-etcd-rpi.sh pi@raspberrypi-old2-eth0:/home/pi/adhoc-cloud/raspi
